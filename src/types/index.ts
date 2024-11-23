d0cba0784c06ee22852ae36fb4c7f6e9c5951aa8
shubhampawar4841 <shubhampawar4036@gmail.com>
2024-11-23 15:32:53 +0530

    error

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,75 +1,75 @@
-import { OutputFileEntry } from '@uploadcare/blocks'
-import { User } from 'firebase/auth'
-
-interface UserSignIn {
-  email: string
-  password: string
-  confirmPassword: string
-}
-interface UserLogIn {
-  email: string
-  password: string
-}
-interface NavItem {
-  name: string
-  link: string
-  icon: (props?: React.SVGProps<SVGSVGElement>) => JSX.Element
-}
-
-interface FileEntry {
-  files: OutputFileEntry[]
-}
-
-interface Post {
-  caption: string
-  photos: PhotoMeta[]
-  likes: number
-  userLikes: string[]
-  userId?: string
-  userName?: string
-  photoURL?: string
-  date: Date
-}
-
-interface PhotoMeta {
-  cdnUrl: string | null
-  uuid: string | null
-}
-
-interface DocumentResponse {
-  id?: string
-  caption?: string
-  photos?: PhotoMeta[]
-  likes?: number
-  userLikes: string[]
-  userId?: string
-  userName?: string
-  photoURL?: string
-  date?: Date
-}
-
-interface LikesInfo {
-  likes?: number
-  isLike?: boolean
-}
-
-interface ProfileInfo {
-  user?: User
-  displayName?: string
-  photoURL?: string
-}
-
-interface UserProfile {
-  userId?: string
-  displayName?: string
-  photoURL?: string
-  userBio?: string
-}
-
-interface UserProfileResponse {
-  id?: string
-  userId?: string
-  displayName?: string
-  photoURL?: string
-  userBio?: string
-}
+export interface UserSignIn {
+    email: string;
+    password: string;
+    confirmPassword: string;
+  }
+  
+  export interface UserLogIn {
+    email: string;
+    password: string;
+  }
+  
+  export interface NavItem {
+    name: string;
+    link: string;
+    icon: (props?: React.SVGProps<SVGSVGElement>) => JSX.Element;
+  }
+  
+  export interface FileEntry {
+    files: OutputFileEntry[];
+  }
+  
+  export interface Post {
+    caption: string;
+    photos: PhotoMeta[];
+    likes: number;
+    userLikes: string[];
+    userId?: string;
+    userName?: string;
+    photoURL?: string;
+    date: Date;
+  }
+  
+  export interface PhotoMeta {
+    cdnUrl: string | null;
+    uuid: string | null;
+  }
+  
+  export interface DocumentResponse {
+    id?: string;
+    caption?: string;
+    photos?: PhotoMeta[];
+    likes?: number;
+    userLikes: string[];
+    userId?: string;
+    userName?: string;
+    photoURL?: string;
+    date?: Date;
+  }
+  
+  export interface LikesInfo {
+    likes?: number;
+    isLike?: boolean;
+  }
+  
+  export interface ProfileInfo {
+    user?: User;
+    displayName?: string;
+    photoURL?: string;
+  }
+  
+  export interface UserProfile {
+    userId?: string;
+    displayName?: string;
+    photoURL?: string;
+    userBio?: string;
+  }
+  
+  export interface UserProfileResponse {
+    id?: string;
+    userId?: string;
+    displayName?: string;
+    photoURL?: string;
+    userBio?: string;
+  }
+  
