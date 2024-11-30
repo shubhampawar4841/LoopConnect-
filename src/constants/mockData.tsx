c22df0acc0c15b92f87661dddf9f6c4be80537b3
shubhampawar4841 <shubhampawar4036@gmail.com>
2024-11-30 19:44:53 +0530

    mock

--- src/constants/mockData.tsx
+++ src/constants/mockData.tsx
@@ -1,37 +1,50 @@
-export const navItems: NavItem[] = [
-  {
-    name: 'Home',
-    link: '/',
-    icon: (props?: React.SVGProps<SVGSVGElement>) => <House {...props} />
-  },
-  {
-    name: 'Add Photos',
-    link: '/post',
-    icon: (props?: React.SVGProps<SVGSVGElement>) => <SquarePlus {...props} />
-  },
-  {
-    name: 'My Photos',
-    link: '/myphotos',
-    icon: (props?: React.SVGProps<SVGSVGElement>) => <Camera {...props} />
-  },
-  {
-    name: 'Profile',
-    link: '/profile',
-    icon: (props?: React.SVGProps<SVGSVGElement>) => <CircleUserRound {...props} />
-  },
-  {
-    name: 'Notifications',
-    link: '#',
-    icon: (props?: React.SVGProps<SVGSVGElement>) => <Bell {...props} />
-  },
-  {
-    name: 'Direct',
-    link: '#',
-    icon: (props?: React.SVGProps<SVGSVGElement>) => <SendHorizontal {...props} />
-  },
-  {
-    name: 'Settings',
-    link: '#',
-    icon: (props?: React.SVGProps<SVGSVGElement>) => <Settings {...props} />
-  }
-]
+import {
+    Bell,
+    Camera,
+    CircleUserRound,
+    House,
+    SendHorizontal,
+    Settings,
+    SquarePlus
+  } from 'lucide-react'
+  
+  //Types
+  import { NavItem } from '@/types/index'
+  
+  export const navItems: NavItem[] = [
+    {
+      name: 'Home',
+      link: '/',
+      icon: (props?: React.SVGProps<SVGSVGElement>) => <House {...props} />
+    },
+    {
+      name: 'Add Photos',
+      link: '/post',
+      icon: (props?: React.SVGProps<SVGSVGElement>) => <SquarePlus {...props} />
+    },
+    {
+      name: 'My Photos',
+      link: '/myphotos',
+      icon: (props?: React.SVGProps<SVGSVGElement>) => <Camera {...props} />
+    },
+    {
+      name: 'Profile',
+      link: '/profile',
+      icon: (props?: React.SVGProps<SVGSVGElement>) => <CircleUserRound {...props} />
+    },
+    {
+      name: 'Notifications',
+      link: '#',
+      icon: (props?: React.SVGProps<SVGSVGElement>) => <Bell {...props} />
+    },
+    {
+      name: 'Direct',
+      link: '#',
+      icon: (props?: React.SVGProps<SVGSVGElement>) => <SendHorizontal {...props} />
+    },
+    {
+      name: 'Settings',
+      link: '#',
+      icon: (props?: React.SVGProps<SVGSVGElement>) => <Settings {...props} />
+    }
+  ]
